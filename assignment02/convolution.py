cccfd4dec9d854cf84468af4ef82257499e4d2cd
Mir Jeffres <smjeff515@gmail.com>
2021-02-11 22:11:23 -0500

    code fix

--- assignment02/convolution.py
+++ assignment02/convolution.py
@@ -5,8 +5,7 @@ import scipy.signal as spsig
 import matplotlib.pyplot as plt
 import time
 
-# x= np.array([1.,2.,3.,4.,5.])
-# h = np.array([99.,99.,99.])
+
 x = np.ones(200)
 h = np.concatenate((np.arange(0,25)/25, np.arange(25,0,-1)/25 ))
 h = np.append(h,0)
@@ -14,25 +13,27 @@ h = np.append(h,0)
 
 #If the length of 'x' is 200 and the length of 'h' is 100, length of 'y' = 299
 def myTimeConv(x,h):
+    y = np.zeros(len(x) + len(h) - 1)
     h = np.flip(h) 
 
     lenX = len(x)
     lenH = len(h)
-    newX = np.concatenate((np.zeros(lenH),x))
-    newH = np.concatenate((h,np.zeros(lenX)))
+    shortest = min(lenX,lenH)
+    longest = max(lenX,lenH)    
+    
 
-    tau = np.arange(0,len(x)+len(h)) #define a list of counting how many values will be calculated
-    sumArr = np.empty([len(x) + len(h), len(x) + len(h)]) #define an empty array to hold intermediate values
+    for t in range(len(y)):
+        if t<longest:
+            curRange = min(t+1,shortest)
+        else:
+            curRange = shortest-(t+1-longest)
 
-    for t in tau:
-        row = newX*newH
-        newH = np.insert(newH,0,0) #inserts a 0 at the beginning of h 
-        newH = newH[:-1] #remove the last value of h so that the arrays stay the same size
-        sumArr[t,:] = row #place the row of index-multiplied values into a new row in the summing 2d array
+        xStart = max(0,t-lenH+1)
+        xEnd = xStart + curRange
+        hStart = max(lenH-1-t,0)
+        hEnd = hStart+curRange
 
-    y = np.sum(sumArr,axis=1) #we have a 2d array, now sum the columns (like doing the integral)
-    y = y[1:] #remove first value which is 0
-
+        y[t] = np.sum(x[xStart:xEnd]*h[hStart:hEnd])
     return y
 
 
@@ -45,10 +46,6 @@ plt.xlabel("tau")
 plt.ylabel("Amplitude")
 plt.savefig('assignment02/results/01-convolution.png')
 plt.show()
-#next to do for future me:
-#In your main script define 'x' as a DC signal of length 200 (constant amplitude of 1) and 'h' as a symmetric triangular signal of length 51 (0 at the first and last sample and 1 in the middle). 
-# Add a function call to myTimeConv() in your script to compute 'y_time' as the time-domain convolution of 'x' and 'h' as defined above. 
-# Plot the result (label the axes appropriately) and save in the results folder [10]
 
 
 #(m, mabs, stdev, time) = 
@@ -58,7 +55,6 @@ def compareConv(x, h):
     t2 = time.perf_counter()
     spOutput = spsig.convolve(x,h)
     t3 = time.perf_counter()
-
     m = np.sum(myOutput-spOutput) / len(myOutput)
     mabs = np.sum(np.abs(myOutput-spOutput)) / len(myOutput)
     stdev = np.sqrt((np.sum((np.abs(myOutput-spOutput)**2)))/len(myOutput))
@@ -76,4 +72,7 @@ print(mabs)
 print(stdev)
 print(times)
 
+txtFile = open("assignment02/results/comparison","w")
+txtFile.writelines(f'Mean: {m}\nMean Absolute Difference: {mabs} \nStandard Deviation: {stdev} \nTime of my function: {times[0]} \nTime of SciPy convolve: {times[1]}')
+txtFile.close()
 
